f5fb713595ee9167a7c51a4f1143561708a3c618
Nelli15 <ellis.nick96@gmail.com>
2022-07-12 10:28:18 +1000

    add build script

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,13 +10,13 @@ initializeApp({
 exports.mt = require("./movementtracker");
 exports.users = require("./users.js");
 
-const converter = require("./movementtracker/converter");
-exports.converter = functions
-  .runWith({
-    // Ensure the function has enough memory and time
-    // to process large files
-    timeoutSeconds: 540,
-    memory: "4GB",
-  })
-  .firestore.document("/forceupdate/true")
-  .onCreate(converter());
+// const converter = require("./movementtracker/converter");
+// exports.converter = functions
+//   .runWith({
+//     // Ensure the function has enough memory and time
+//     // to process large files
+//     timeoutSeconds: 540,
+//     memory: "4GB",
+//   })
+//   .firestore.document("/forceupdate/true")
+//   .onCreate(converter());
