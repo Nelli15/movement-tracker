3d7ce53ac37a9043623de61fd249c1f194cf74da
Nelli15 <ellis.nick96@gmail.com>
2022-07-25 12:33:17 +1000

    remove converter function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as functions from "firebase-functions";
+// import * as functions from "firebase-functions";
 
 import { initializeApp, applicationDefault } from "firebase-admin/app";
 
@@ -10,13 +10,13 @@ initializeApp({
 exports.mt = require("./movementtracker");
 exports.users = require("./users.js");
 
-const converter = require("./movementtracker/converter");
-exports.converter = functions
-  .runWith({
-    // Ensure the function has enough memory and time
-    // to process large files
-    timeoutSeconds: 540,
-    memory: "4GB",
-  })
-  .firestore.document("/forceupdate/true")
-  .onCreate(converter());
+// const converter = require("./movementtracker/converter");
+// exports.converter = functions
+//   .runWith({
+//     // Ensure the function has enough memory and time
+//     // to process large files
+//     timeoutSeconds: 540,
+//     memory: "4GB",
+//   })
+//   .firestore.document("/forceupdate/true")
+//   .onCreate(converter());
